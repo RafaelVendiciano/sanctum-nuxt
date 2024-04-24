0d37d431059e8d87591805ec4c7ffe0cf10898c9
Rafael Barbosa <rafaelvbarbosa3@gmail.com>
2024-04-24 17:44:42 -0300

    logout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,53 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+    const user = ref({})
+    await $fetch("http://localhost:8000/sanctum/csrf-cookie", { 
+          credentials: "include"
+        })
+
+    const token = useCookie('XSRF-TOKEN')
+
+    async function loadUser(){
+      const response = await $fetch("http://localhost:8000/api/user", {
+            credentials:"include",
+            redirect: "manual",
+            watch: false,
+            ignoreResponseError: true,
+            headers: {
+              'X-XSRF-TOKEN': token.value as string
+            }
+        })
+
+        user.value = response as object
+    }
+
+    async function handleLogout(){
+        await $fetch("http://localhost:8000/sanctum/csrf-cookie", { 
+            credentials: "include"
+        })
+
+        const token = useCookie('XSRF-TOKEN')
+
+        await $fetch("http://localhost:8000/api/logout", {
+            credentials:"include",
+            method: "POST",
+            watch: false,
+            headers: {
+              'X-XSRF-TOKEN': token.value as string
+            }
+        })
+
+        navigateTo('/')
+
+    }
+
+
+</script>
 
 <template>
   <div>
+    <button @click="loadUser">Load User</button>
+    <button v-if="user" @click="handleLogout">Logout</button>
+    <pre> {{ user }} </pre>
     <ul>
       <li><NuxtLink to="/">Home</NuxtLink></li>
       <li><NuxtLink to="/login">Login</NuxtLink></li>
